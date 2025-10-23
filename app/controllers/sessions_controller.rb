253e2cab78a6d23d12bc0bd10fce6c21b6eefa9a
Julian Beldotti <julian.beldotti@gmail.com>
2025-10-23 15:29:35 -0600

    Adds a new session controller and create action

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def create
+    # user = User.find_or_create_by(email: :email)
+    
+    # session[:user_id] = user.id
+
+    # redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
