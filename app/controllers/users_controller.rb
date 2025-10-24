c2965d9a67e494988df8474f4d9ea982f8f86d27
Julian Beldotti <julian.beldotti@gmail.com>
2025-10-24 15:03:23 -0600

    Updates users controller with create and show action as well as remove lines from login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,42 @@
 class UsersController < ApplicationController
 
   def login_form
-
   end
 
   def login
-    # require 'pry'; binding.pry
     user = User.find_by(email: params[:email])
-    # user = current_user
+
     if user.authenticate(params[:password])
-      # require 'pry'; binding.pry
-    # if user.present?
       flash[:success] = "Welcome #{user.name}"
       session[:user_id] = user.id
+
       redirect_to root_path
     else
       flash[:alert] = "Sorry, wrong credentials"
+
       redirect_to login_path
     end
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "Welcome to Priorities #{user.name}"
+      redirect_to user_path(user)
+    else
+      flash[:alert] = "Bruh, you done fuq'd up. Try again."
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    user = User.find_by(name: params[:name])
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
