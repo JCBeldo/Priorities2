93929216e5cf0030999f20f609ffadc9a2feb537
Julian Beldotti <julian.beldotti@gmail.com>
2025-10-26 11:52:51 -0600

    Updates schema with new submission tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_20_220212) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_26_032224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "submission_set_entries", force: :cascade do |t|
+    t.bigint "submission_set_id", null: false
+    t.bigint "submission_id", null: false
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["submission_id"], name: "index_submission_set_entries_on_submission_id"
+    t.index ["submission_set_id"], name: "index_submission_set_entries_on_submission_set_id"
+  end
+
+  create_table "submission_sets", force: :cascade do |t|
+    t.bigint "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_submission_sets_on_owner_id"
+  end
 
   create_table "submissions", force: :cascade do |t|
     t.text "content"
@@ -34,5 +51,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_20_220212) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "submission_set_entries", "submission_sets"
+  add_foreign_key "submission_set_entries", "submissions"
+  add_foreign_key "submission_sets", "users", column: "owner_id"
   add_foreign_key "submissions", "users"
 end
